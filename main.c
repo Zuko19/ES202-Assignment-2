93d526efb175c26bb00e659007512ee3c8f8a14e
Zuko19 <75013060+Zuko19@users.noreply.github.com>
2020-11-29 01:28:20 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+//Program to print all alphabets
+#include <stdio.h>
+
+void main()
+{
+    int a = 97;
+    char c;
+    for(a = 97; a<123; a++)
+    {
+        c = a;
+        printf("%c \n",c);
+    }
+}
+
+
